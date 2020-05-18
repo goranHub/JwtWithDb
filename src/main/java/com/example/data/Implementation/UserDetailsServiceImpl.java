b8b485461526676bc28fbea3d53ee450feeb901b
gsico <goran22sico@gmail.com>
2020-05-18 08:21:36 +0200

    firstVersion

---
+++ src/main/java/com/example/data/Implementation/UserDetailsServiceImpl.java
@@ -0,0 +1,71 @@
+package com.example.data.Implementation;
+
+
+
+import com.example.data.RoleDataAccessService;
+import com.example.data.UserDataAccessService;
+import com.example.model.Role;
+import com.example.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Qualifier("userDetailsService")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserDataAccessService userDataRepository;
+    @Autowired
+    private RoleDataAccessService roleDataAccessService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserDetailsManagerImpl userDetailsManager;
+    @Autowired
+    User user;
+    @Autowired
+    Role role;
+
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+
+        user = userDataRepository.loadUserByUsername(username);
+
+
+        if (user.getFname().equals(username)) {
+            if(username.equals("admin")){
+                user.setRole("ADMIN");
+                userDetailsManager.createUser(user);
+                roleDataAccessService.setRoleAdmin(role,user);
+                return user;
+            }
+
+
+            if(username.equals("develop")){
+                user.setRole("DEVELOPER");
+                userDetailsManager.createUser(user);
+                roleDataAccessService.setRoleDeveloper(role, user);
+
+                return user;
+            }
+
+        }
+
+
+        return null;
+    }
+
+
+}
+
+
+
+
